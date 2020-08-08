2cc0e2650941927d6f6df3649335c261cb13e97f
rw00 <raafatwahb@gmail.com>
2020-08-08 13:47:17 +0300

    Adder app with undo and redo operations

---
+++ src/main/java/com/rw/simple/random/apps/MainRunner.java
@@ -0,0 +1,13 @@
+package com.rw.simple.random.apps;
+
+public class MainRunner {
+    private MainRunner() {
+    }
+
+
+    // driver
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        adder.run();
+    }
+}
